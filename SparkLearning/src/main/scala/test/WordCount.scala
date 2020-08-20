8786a1ec47cc30367d8fcf1c96bc46df35d1c1b1
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2020-08-20 10:34:18 +0530

    WirdCount program with proper regex for fetching only words

---
+++ SparkLearning/src/main/scala/test/WordCount.scala
@@ -0,0 +1,22 @@
+package test
+import org.apache.spark.SparkContext
+import org.apache.log4j._
+
+object WordCount  extends App{
+
+  Logger.getLogger("org").setLevel(Level.ERROR)
+
+  val sc = new SparkContext("local[*]","test")
+  val book = sc.textFile("src/data/book.txt")
+  val words = book.flatMap(x => x.split("\\W+"))
+  val Lowercase = words.map(x => x.toLowerCase())
+
+  val mappedWords = Lowercase.map(x => (x,1)).reduceByKey((x,y) => x+y)
+  val sortedWords = mappedWords.map(x => (x._2,x._1)).sortByKey()
+
+  val finalWords = sortedWords.filter(x => x._2!="the" && x._2!="a" && x._2!="an" && x._2!="you" && x._2!="that" && x._2!="then" && x._2!="does" && x._2!="do" && x._2!="did" && x._2!="done" && x._2!="or" && x._2!="and" )
+  finalWords.foreach(println)
+  println( "before filter " + sortedWords.count())
+  println("after filter " + finalWords.count())
+
+}
