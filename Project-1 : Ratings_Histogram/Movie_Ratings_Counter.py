82d117a8c79b5a4fd3edec08f861a65afccb21d3
Vishal Barvaliya <vishalbarvaliya112@gmail.com>
2021-05-08 09:38:20 +0530

    in this spark project we found that how much  1 star ratings and 2 star ratings and so on and so forth.
    in this project in data directory data is given for 100k movie ratings on which we are doing analysis

---
+++ Project-1 : Ratings_Histogram/Movie_Ratings_Counter.py
@@ -0,0 +1,12 @@
+from pyspark import SparkConf, SparkContext
+import collections
+
+conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+sc = SparkContext(conf = conf)
+lines = sc.textFile("/home/vishal/D/Data/A sparkscala/ml-100k/u.data")
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.items():
+    print(key, value)
