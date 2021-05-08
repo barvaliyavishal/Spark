ae9ab68ae972e559d35e4b774ce45950c3afdaf4
Vishal Barvaliya <vishalbarvaliya112@gmail.com>
2021-05-08 17:55:32 +0530

    Minimum Temperature
    in this project we have dataset of weather station of year 1800 and we need to find Minimum temerature for each station

---
+++ Project-3: Minimum_Temperature_for_Each_Weather_Station/Minimum_Temperature.py
@@ -0,0 +1,21 @@
+from pyspark import SparkContext, SparkConf
+
+conf = SparkConf().setMaster("local").setAppName("Minimum_Temperature")
+sc = SparkContext(conf=conf)
+
+def parseline(line):
+    fields = line.split(",")
+    station = fields[0]
+    temp = int(fields[3])
+    type = fields[2]
+    return (station,temp, type)
+
+lines = sc.textFile("/home/vishal/D/Udemy/frank kane/PySpark/2. Spark Basics and the RDD Interface/1800.csv")
+rdd = lines.map(parseline)
+
+minTemps = rdd.filter(lambda x : "TMIN" in x[2])
+stationTemps = minTemps.map(lambda x :(x[0],int(x[1])))
+finalMin = stationTemps.reduceByKey(lambda x,y : min(x,y))
+res = finalMin.collect()
+for i in res:
+    print(i)
